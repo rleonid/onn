77bfe1bc56d3caf652f9ab9aacac4da608080cae
Leonid Rozenberg <leonidr@gmail.com>
2015-09-17 23:33:53 -0400

    Add download script.

---
+++ mnist/curl_datasets.sh
@@ -0,0 +1,4 @@
+curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz | gunzip -c > t10k-images-idx3-ubyte
+curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz | gunzip -c > t10k-labels-idx1-ubyte
+curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz | gunzip -c > train-images-idx3-ubyte
+curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz | gunzip -c > train-labels-idx1-ubyte
