48b357893e851a08e7256246a74af5eb8bfb6838
Leonid Rozenberg <leonidr@gmail.com>
2015-09-28 19:08:46 -0400

    Add regularization test and performance.

--- src/test.ml
+++ src/test.ml
@@ -4,11 +4,11 @@ let simple_t () = Onn.do_it ~batch_size:50 ~num_hidden_nodes:20 ~epochs:30 ~lear
 let () =
   let t =
     Onn.do_it
-      ~cache:true
       ~batch_size:10
       ~num_hidden_nodes:100
       ~epochs:30
       ~learning_rate:0.5
+      ~lambda:5.0
       Onn.cross_entropy_cdf
   in
   let test_d = Load_mnist.data `Test in
