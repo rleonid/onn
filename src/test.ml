bbc58f92ae1a3c8182d9d2b33d1497fea33709b2
Leonid Rozenberg <leonidr@gmail.com>
2015-09-28 12:02:46 -0400

    Clear up hidden layers variable name.

--- src/test.ml
+++ src/test.ml
@@ -1,5 +1,5 @@
 
-let simple_t () = Onn.do_it ~batch_size:50 ~hidden_layers:20 ~epochs:30 ~learning_rate:0.3
+let simple_t () = Onn.do_it ~batch_size:50 ~num_hidden_nodes:20 ~epochs:30 ~learning_rate:0.3
 
 let () =
   let t =
@@ -7,7 +7,7 @@ let () =
       ~cache:true
       ~iterative:false
       ~batch_size:10
-      ~hidden_layers:100
+      ~num_hidden_nodes:100
       ~epochs:30
       ~learning_rate:0.5
   in
