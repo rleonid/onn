ec1d67e4143911da33ba97f73e9f43829d8eeec9
Leonid Rozenberg <leonidr@gmail.com>
2015-10-02 20:44:26 -0400

    Initial crawlings to load CIFAR 10 data.

---
+++ src/load_cifar.ml
@@ -0,0 +1,28 @@
+(* Load CIFAR-10 data
+ * http://www.cs.toronto.edu/~kriz/cifar.html
+ *)
+
+let train_batch1_fname = "data/cifar_10/cifar-10-batches-bin/data_batch_1.bin"
+let train_batch1_fname = "data/cifar_10/cifar-10-batches-bin/data_batch_2.bin"
+let train_batch1_fname = "data/cifar_10/cifar-10-batches-bin/data_batch_3.bin"
+let train_batch1_fname = "data/cifar_10/cifar-10-batches-bin/data_batch_4.bin"
+let train_batch1_fname = "data/cifar_10/cifar-10-batches-bin/data_batch_5.bin"
+let test_batch_fname = "data/cifar_10/cifar-10-batches-bin/test_batch.bin"
+
+let parse_file ~d ~t fname =
+  let fd = Unix.openfile fname [Unix.O_RDONLY] 0o600 in
+  let c = 10000 in
+  let r = 3 * 32 * 32 in (* 3 for R, G, B *)
+  let a = Array2.map_file fd Int8_unsigned C_layout false c r in
+  let labels = Array1.create d Fortran_layout c in
+  let values = Array2.create d Fortran_layout c (r - 1) in
+  for i = 0 to c - 1 do
+    labels.{i + 1} <- t (Array2.get a i 0);
+    for j = 1 to r - 1 do  
+      values.{i + 1, j} <- t (Array2.get a i j)
+    done;
+  done;
+  labels, values
+
+let parse_file_raw = parse_file ~d:Int8_unsigned ~t:(fun x -> x) 
+let parse_file_float = parse_file ~d:Float64 ~t:float_of_int
